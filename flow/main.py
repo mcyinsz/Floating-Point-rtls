e9e8ed72c231e6f55b40abeda015a9b75f992ca7
mcyinsz <yihan_yin@163.com>
2025-06-25 18:06:39 +0800

    update document

--- flow/main.py
+++ flow/main.py
@@ -192,7 +192,7 @@ def main():
     """
 
     # get run dir name
-    top_module = 'fp32_naive_adder'
+    top_module = 'fp32_naive_multiplier'
 
     # initialize run_dir
     os.makedirs(RESULT_DIR, exist_ok=True)
