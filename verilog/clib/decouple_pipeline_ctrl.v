93c118bb80ba5212ddbb1db7fe21cefec8921672
mcyinsz <yihan_yin@163.com>
2025-06-26 14:36:27 +0800

    correct decouple pipeline ctrl

--- verilog/clib/decouple_pipeline_ctrl.v
+++ verilog/clib/decouple_pipeline_ctrl.v
@@ -14,16 +14,25 @@ module decouple_pipeline_ctrl (
     // ctrl logic
     // empty or dequeue
     assign in_ready =  (!out_valid) || (out_ready);
+    
+    // in/out fire
+    wire in_fire = in_ready && in_valid;
+    wire out_fire = out_ready && out_valid;
 
     always @(posedge clock or posedge reset) begin
         if (reset) begin
             out_valid <= 1'b0;
         end else begin
-            if (in_ready) begin
-                out_valid <= in_valid;
+            if (out_fire && (!(in_fire))) begin // input fire without input
+                out_valid <= 1'b0;
+            end 
+            else if (in_fire && (!out_fire)) begin // output fire without input
+                out_valid <= 1'b1;
+            end
+            else begin
+                out_valid <= out_valid;
             end
         end
     end
-
 
 endmodule
