59e4456270084766cb6d5e88b90eba1741cc9ae8
mcyinsz <yihan_yin@163.com>
2025-07-10 02:00:24 +0800

    merge compare/div ppa into repo

--- scripts/ModulePPA.py
+++ scripts/ModulePPA.py
@@ -6,20 +6,20 @@ MULT_AREA = 6922.351
 MULT_POWER = 0.0021016
 
 # FP32 adder
-ADD_AREA = 4126.023
-ADD_POWER = 0.00117034
+ADD_AREA = 4126.023 # 2309.472
+ADD_POWER = 0.00117034 # 0.00053673
 
 # FP32 divider
-DIV_AREA = 6922.351
-DIV_POWER = 1e-3
+DIV_AREA = 4391.263
+DIV_POWER = 0.00145984
 
 # FP32 compare
-COMPARE_AREA = 1000
-COMPARE_POWER = 1e-4
+COMPARE_AREA = 395.41
+COMPARE_POWER = 6.88259e-05
 
 # SRAM per bit
-SRAM_BITAREA = 60000/(300 * 1e3)
-SRAM_BITPOWER = 1e-3 / (0.12 * 1e3)
+SRAM_BITAREA = 60000/(300 * 1e3) * (7/28)**2 # 28nm -> 7nm
+SRAM_BITPOWER = 1e-3 / (0.12 * 1e3) # TODO: scale this param
 
 # ========================================================
 
@@ -44,3 +44,13 @@ SRAM_BITPOWER = 1e-3 / (0.12 * 1e3)
 # "Post-Syn Timing": 398,
 # "Post-Syn Power": 0.0021016,
 # "Post-Syn Area": 6922.351,
+
+# fp32 naive compare unit
+# "Post-Syn Timing": 123,
+# "Post-Syn Power": 6.88259e-05,
+# "Post-Syn Area": 395.41,
+
+# fp32 naive divider unit
+# "Post-Syn Timing": 191,
+# "Post-Syn Power": 0.00145984,
+# "Post-Syn Area": 4391.263,
